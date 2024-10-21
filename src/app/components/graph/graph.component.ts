8807fa0cc10d0ed557c36debea0ac2a2dc871165
Venkat-31 <venkat.itofficial@gmail.com>
2024-10-21 15:54:23 +0530

    latest copy

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -100,54 +100,12 @@ export class GraphComponent {
       .append("rect")
       .attr("width", this.width)
       .attr("height", this.height)
-      .attr("fill", "lightgray");
+      .attr("fill", "#e8e8e8");
 
     // Add axes
     this.svg
       .append("g")
       .call(d3.axisTop(this.xScale).tickFormat(d3.format("d")));
-
-    // // Create the y-axis and get the ticks
-    // const yAxis = d3.axisLeft(this.yScale);
-    // this.svg.append("g").call(yAxis); // Draw the y-axis
-
-    // // Get the domain (the entity names) for custom ticks
-    // const yTicks = this.yScale.domain();
-
-    // // Draw custom ticks before y-axis ticks
-    // this.svg
-    //   .selectAll(".custom-tick")
-    //   .data(yTicks)
-    //   .enter()
-    //   .append("line")
-    //   .attr("class", "custom-tick")
-    //   .attr("x1", -10) // Length of the custom tick
-    //   .attr("x2", 0) // End point of the custom tick
-    //   .attr("y1", (d: string) => (this.yScale(d) as number) + this.yScale.bandwidth() / 2) // Center vertically
-    //   .attr("y2", (d: string) => (this.yScale(d) as number) + this.yScale.bandwidth() / 2) // Center vertically
-    //   .attr("stroke", "black")
-    //   .attr("stroke-width", 1);
-    
-    // // X-axis with ticks
-    // this.svg
-    //   .append("g")
-    //   .attr("transform", `translate(0, 0)`)
-    //   .call(d3.axisTop(this.xScale).tickFormat(d3.format("d")));
-
-    // // Draw grid lines for x-axis
-    // this.svg
-    //   .append("g")
-    //   .attr("class", "x-grid")
-    //   .selectAll("line")
-    //   .data(this.uniqueYears)
-    //   .enter()
-    //   .append("line")
-    //   .attr("x1", (d: number) => (this.xScale(d) as number) + this.xScale.bandwidth() / 2)
-    //   .attr("x2", (d: number) => (this.xScale(d) as number) + this.xScale.bandwidth() / 2)
-    //   .attr("y1", 0)
-    //   .attr("y2", this.height)
-    //   .attr("stroke", "#a3a3a3")
-    //   .attr("stroke-opacity", 0.7);
 
     this.svg.append("g").attr("class", "custom-tick").call(d3.axisLeft(this.yScale));
     this.svg
@@ -191,7 +149,6 @@ export class GraphComponent {
     const threadOffset: Record<string, number> = {};
     const threadPositions: Record<string, { x: number; y: number }> = {};
     let xOffsetPoint: { x: number; crossThread: number }[] = [];
-    let yOffsetPoint: { y: number; crossThread: number }[] = [];
 
     // Draw circles with slight offsets for overlapping threads
     groupedData.forEach(([year, entities]) => {
@@ -259,7 +216,7 @@ export class GraphComponent {
       }
 
       const lineColor =
-        relation.tieringType === TieringType.Real ? "red" : "yellow";
+        relation.tieringType === TieringType.Real ? "#e7377b" : "#eab005";
       let x1 = threadPositions[lowerThread.thread].x;
       const y1 =
         (this.yScale(lowerThread.entity) as number) +
@@ -328,91 +285,6 @@ export class GraphComponent {
         .attr("stroke-width", 0.75)
         .attr("marker-end", "url(#arrow)");
     });
-
-    // this.rollForwardRelations.forEach((relation) => {
-    //   if (!relation.rollForward) {
-    //     return;
-    //   }
-    //   const mainThread = this.entityThreads.find(
-    //     (t) => t.thread === relation.mainThread
-    //   ) as EntityThread;
-    //   const forwardThread = this.entityThreads.find(
-    //     (t) => t.thread === relation.rollForward
-    //   ) as EntityThread;
-
-    //   if (!mainThread || !forwardThread) {
-    //     return;
-    //   }
-
-    //   const lineColor =
-    //     relation.type === TieringType.Real ? '#283379' : '#4cbaeb';
-    //   const x1 = threadOffset[mainThread.thread];
-    //   const y1 = (this.yScale(mainThread.entity) as number) + this.yScale.bandwidth() / 2;
-    //   const x2 = threadOffset[forwardThread.thread];
-    //   const y2 =
-    //     (this.yScale(forwardThread.entity) as number) + this.yScale.bandwidth() / 2;
-    //   console.log("mainThread",mainThread);
-
-    //   const pointIndex = yOffsetPoint.findIndex((point) => point.y === y1);
-    //   console.log("threadPositions",threadPositions);
-    //   const threadCrossThread = Object.values(threadPositions).some((point) => point.y === y1 && isNumberInRange(point.x, x1 + 1, x2 - 1));
-    //   console.log("threadCrossThread ",threadCrossThread, yOffsetPoint, pointIndex);
-    //   let lineData: { x: number; y: number }[] = [];
-
-    //   if (threadCrossThread) {
-    //     lineData = [
-    //       { x: x1, y: y1 + 5 },
-    //       { x: x1 + 20, y: y1 + 15 }, // Control point for the curve
-    //       { x: x1 + 50, y: y1 + 15 },
-    //       { x: x2 - 20, y: y2 + 15 }, // Control point for the curve
-    //       { x: x2, y: y2 + 5 },
-    //     ];
-    //     const currentCrossThread = (yOffsetPoint[pointIndex]?.crossThread || 1);
-    //     lineData[1].y += currentCrossThread * 10;
-    //     lineData[2].y += currentCrossThread * 10;
-    //     lineData[3].y += currentCrossThread * 10;
-    //     if (!yOffsetPoint[pointIndex]?.crossThread) {
-    //       yOffsetPoint.push({
-    //         y: y1,
-    //         crossThread: 2,
-    //       });
-    //     } else {
-    //       yOffsetPoint[pointIndex].crossThread += 1;
-    //     }
-    //     // yOffsetPoint[pointIndex].crossThread += 1;
-    //   } else {
-    //     lineData = [
-    //       { x: x1 + 5, y: y1 },
-    //       { x: x2 - 5, y: y2 },
-    //     ];
-    //     yOffsetPoint.push({
-    //       y: y1,
-    //       crossThread: 0,
-    //     });
-    //   }
-
-    //   this.svg
-    //     .append('defs')
-    //     .append('marker')
-    //     .attr('id', 'arrow')
-    //     .attr('viewBox', '0 0 10 10')
-    //     .attr('refX', 10) // Position of the arrowhead
-    //     .attr('refY', 5)
-    //     .attr('markerWidth', 6)
-    //     .attr('markerHeight', 6)
-    //     .attr('orient', 'auto')
-    //     .append('polygon')
-    //     .attr('points', '0,0 10,5 0,10')
-    //     .attr('fill', lineColor);
-    //   this.svg
-    //     .append('path')
-    //     .datum(lineData)
-    //     .attr('d', lineGenerator)
-    //     .attr('fill', 'none')
-    //     .attr('stroke', lineColor)
-    //     .attr('stroke-width', 0.75)
-    //     .attr('marker-end', 'url(#arrow)');
-    // });
 
     // Define an arrow marker for the end of the line
     this.svg
